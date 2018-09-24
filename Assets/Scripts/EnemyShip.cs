015d66a5844f67900d2367177a5239f641d48dce
Anton Debner <anton.debner@aalto.fi>
2018-09-24 16:12:17 +0300

    Implement EnemyShip and a simple Projectile
    Former-commit-id: 5cf5d32f75ea7a73dcac5857fd82e46a25b04615

---
+++ Assets/Scripts/EnemyShip.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShip : MonoBehaviour
+{
+    public float maxSpeed = 1.0f;
+    public float maxAcceleration;
+    public float rateOfFire = 1.0f;
+    public float degreesBetweenShots = 30.0f;
+    public Rigidbody projectilePrefab;
+    public GameObject movementEndPoint;
+
+    private Vector3 startPoint;
+    private Vector3 endPoint;
+
+    private float shootingAngle;
+    private float previousShotTime;
+    private float startTime;
+    private float movementLength;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    startPoint = transform.position;
+	    endPoint = movementEndPoint.transform.position;
+	    movementEndPoint.SetActive(false);
+        startTime = Time.time;
+	    previousShotTime = Time.time;
+        movementLength = Vector3.Distance(startPoint, endPoint);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    // Move towards target position
+	    var distanceTraveled = (Time.time - startTime) * maxSpeed;
+	    var fraction = distanceTraveled / movementLength;
+	    transform.position = Vector3.Lerp(startPoint, endPoint, fraction);
+
+	    // Change direction if necessary
+	    if (fraction >= 1.0f)
+	    {
+	        startTime = Time.time;
+	        var temp = startPoint;
+	        startPoint = endPoint;
+	        endPoint = temp;
+	    }
+
+        // Handle shooting
+	    if (Time.time - previousShotTime > (1 / rateOfFire))
+	    {
+	        previousShotTime = Time.time;
+            Rigidbody projectile = (Rigidbody)Instantiate(projectilePrefab, transform.position, Quaternion.Euler(0, 0, shootingAngle));
+	        projectile.GetComponent<Projectile>().SetToIgnoreEnemyCollisions();  // Enemies cannot kill enemies
+	        shootingAngle += degreesBetweenShots;
+        }
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    public float speed = 10f;
+    public float maxSecondsAlive = 10f;
+
+	// Use this for initialization
+	void Start ()
+	{
+        // Set initial velocity
+	    GetComponent<Rigidbody>().velocity = speed * transform.right;
+	    Destroy(gameObject, maxSecondsAlive);
+    }
+
+    public void SetToIgnorePlayerCollisions()
+    {
+        // Set the projectile to go through the player
+        gameObject.layer = LayerMask.NameToLayer("Player");
+    }
+
+    public void SetToIgnoreEnemyCollisions()
+    {
+        // Set the projectile to go through all enemies
+        gameObject.layer = LayerMask.NameToLayer("Enemy");
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision!");
+        Destroy(gameObject);
+    }
+}
