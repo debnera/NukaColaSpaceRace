b4493561ced755263ec790fcc45ceaa716c873d6
Rauli <rauli.valo@aalto.fi>
2018-09-27 14:05:53 +0300

    Thrust and player shoot
    kaasua ja ammuntaa
    
    
    Former-commit-id: 9a7fcb02264eadf92941d9ec24b070b8f74b0887

---
+++ Assets/PlayerShootAudio.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootAudio : MonoBehaviour
+{
+
+
+    private AudioSource audioSource;
+    public AudioClip playerShoot3;
+
+
+   
+
+
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+           audioSource.PlayOneShot(playerShoot3);
+        }
+    }
+}
