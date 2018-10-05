def6dcd4fef35fbe5f98ba3a825222f778caa94a
Anton Debner <anton.debner@aalto.fi>
2018-10-05 16:06:05 +0300

    Add gate collision sound and remove some debug lines
    Former-commit-id: 637790651ccac86d5c133f1aee7059231ebf96b1

---
+++ Assets/Scripts/Gate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour {
+
+    private AudioSource HitSource;
+
+    void Awake()
+    {
+        HitSource = GetComponent<AudioSource>();
+    }
+
+    void ApplyDamage(int value)
+    {
+        HitSource.Play();
+    }
+}
