03f28e99f10e4d52c72bc2e3b80f19385df8f53f
Kofi <kofi.hudson@aalto.fi>
2018-10-09 13:17:11 +0300

    Score and Destruction
    Added player ship destruction elements
    Added score screen
    
    
    Former-commit-id: 73146172e3f87052bcd364218d0bdc8ac543ac94

---
+++ Assets/Scripts/Boom.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boom : MonoBehaviour {
+
+    public ParticleSystem destructionEffect;
+
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    private void OnCollisionEnter ()
+    {
+        Instantiate(destructionEffect, transform.position, transform.rotation);
+        destructionEffect.Play();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+
+    }
+}
